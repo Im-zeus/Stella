d516c230497ad8f6c79049f969d23abe8e3e8f60
zues <87465532+Im-zeus@users.noreply.github.com>
2021-08-17 08:55:20 +0530

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -7,7 +7,6 @@ from telegram.error import BadRequest
 from telegram.ext import Filters, MessageHandler, run_async
 
 from tg_bot import dispatcher
-from tg_bot.modules.sql.afk_redis import start_afk, end_afk, is_user_afk, afk_reason
 from tg_bot.modules.users import get_user_id
 
 from tg_bot.modules.helper_funcs.alternate import send_message
