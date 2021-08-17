341646c617e5b19e263d954c52fc94baa62b3dab
zues <87465532+Im-zeus@users.noreply.github.com>
2021-08-17 08:43:47 +0530

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -6,14 +6,14 @@ from telegram import MessageEntity, ParseMode
 from telegram.error import BadRequest
 from telegram.ext import Filters, MessageHandler, run_async
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
-from SaitamaRobot.modules.sql.afk_redis import start_afk, end_afk, is_user_afk, afk_reason
-from SaitamaRobot import REDIS
-from SaitamaRobot.modules.users import get_user_id
+from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
+from tg_bot.modules.sql.afk_redis import start_afk, end_afk, is_user_afk, afk_reason
+from tg_bot import REDIS
+from tg_bot.modules.users import get_user_id
 
-from SaitamaRobot.modules.helper_funcs.alternate import send_message
-from SaitamaRobot.modules.helper_funcs.readable_time import get_readable_time
+from tg_bot.modules.helper_funcs.alternate import send_message
+from tg_bot.modules.helper_funcs.readable_time import get_readable_time
 
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
